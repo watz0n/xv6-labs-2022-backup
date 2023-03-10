1c852dfe1f8fbcb8d9bfef0e55ad977491acf625
watz0n <wats0n.edx@gmail.com>
2023-03-10 10:03:45 +0800

    backup

---
+++ kernel/kalloc.c
@@ -0,0 +1,101 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+struct {
+  unsigned char ref[128*1024]; //from memlayout.h, #define PHYSTOP (KERNBASE + 128*1024*1024)
+} pmtbl;
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+
+  for(int i=0; i<128*1024; i=i+1) 
+    pmtbl.ref[i] = 1; //for freerange to free pages in start-up period
+
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
+}
+
+// Free the page of physical memory pointed at by pa,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  pmtbl.ref[(((uint64)pa)>>12)&0x1FFFF] = pmtbl.ref[(((uint64)pa)>>12)&0x1FFFF] ? pmtbl.ref[(((uint64)pa)>>12)&0x1FFFF]-1 : 0 ;
+  
+  if(pmtbl.ref[(((uint64)pa)>>12)&0x1FFFF]) {
+    release(&kmem.lock);
+    return;
+  }
+  
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  //acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  pmtbl.ref[(((uint64)r)>>12)&0x1FFFF] += 1;
+
+  release(&kmem.lock);
+  //printf("pmtbl.ref[0x%x]=%d\n", (((uint64)r)>>12)&0x1FFFF, pmtbl.ref[(((uint64)r)>>12)&0x1FFFF]);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
